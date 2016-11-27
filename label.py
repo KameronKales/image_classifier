902c29cf1e875f99d580480e75dcc01cf31cbe4b
Kameron Kales <kameron@askglance.com>
2016-11-26 23:56:37 -0500

    initial commit

---
+++ label.py
@@ -0,0 +1,7 @@
+# This tests to see which label the model guesses to contain the below picture
+
+    python / tf_files / label_image.py / tf_files / \
+        flower_photos / boxers / pic_001.jpg
+
+# python /tf_files/label_image.py /tf_files/flower_photos/poodles/pic_001.jpg
+# python /tf_files/label_image.py /tf_files/flower_photos/
